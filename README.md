a33f6e68b53718ba8ead410c78c1e7cb0f2d71d7
KP Kaiser <kirk@zothcorp.com>
2019-10-29 16:47:17 -0400

    Add better docs and the lambda layer

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ It uses [Chalice](https://github.com/DataDog/datadog-lambda-layer-python), a Fla
 It's cool because you can do things like respond to SNS notifications or schedule things to run at a certain time in code.
 
 Right now the app doesn't do much, other than get instrumented. That might change soon!
+
+## Running the Example Serverless App Locally
+
+Use virtualenv to separate your Serverless app's requirements:
+
+```bash
+$ pip install virtualenv
+$ virtualenv ~/.virtualenvs/chalice-demo
+$ source ~/.virtualenvs/chalice-demo/bin/activate
+```
+
+Once that's done, you can then do a:
+
+```bash
+$ pip install -r requirements.txt
+$ chalice local
+```
+
+(You can specify a different port with `chalice local --port=9001`)
+
+Send an example request to be reversed with `curl`:
+
+```bash
+$ curl -d '{"string": "Hello!"}' -H "Content-Type: application/json" localhost:8000
+```
+
+
--- app.py
+++ app.py
@@ -1,35 +1,24 @@
 from chalice import Chalice
 
+from datadog_lambda.wrapper import datadog_lambda_wrapper
+from datadog_lambda.metric import lambda_metric
+
 app = Chalice(app_name='swapcaser')
 
-
+@datadog_lambda_wrapper
 @app.route('/', methods=['POST', 'GET'])
 def index():
     request = app.current_request
     if request.method == 'POST':
-        request_string = request.json_body['string']
+        request_string = request.json_body['string'] # expecting {"string": "blahhh"}
+        app.log.info(f"Reverser called with {request_string}")
+
+        lambda_metric("reverse_service.string_length", # metric
+                      len(request_string), # value
+                      tags=['serverless:reverser'])
+
         response = {'response': request_string.swapcase()}
         return response
 
     return {'hello': 'world'}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
